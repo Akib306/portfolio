9604fa1fc7dcb36292caea28cde38e51ba835625
Motasin Akib <306akib@gmail.com>
2026-05-01 15:15:05 -0600

    fix: formatting

--- prettier.config.js
+++ prettier.config.js
@@ -2,9 +2,11 @@
 
 /** @type {import('prettier').Config} */
 const config = {
-  semi: false,
-  singleQuote: true,
-  trailingComma: "all",
-};
+	semi: false,
+	singleQuote: true,
+	trailingComma: 'all',
+	useTabs: true,
+	tabWidth: 4,
+}
 
-export default config;
+export default config
